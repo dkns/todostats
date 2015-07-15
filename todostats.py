a897fc0c2e1ae7dddf82caab53cc5939e4184192
Daniel Kosinski <de.kosinski@gmail.com>
2015-07-15 21:57:56 +0200

    Fix values

--- todostats.py
+++ todostats.py
@@ -36,28 +36,28 @@ def get_valid_dates(start_date, end_date):
 
     return valid_dates
 
-valid_dates = get_valid_dates(end_date, start_date)
+valid_dates = get_valid_dates(start_date, end_date)
 
-def get_valid_projects(list_of_days, filename):
-    found_projects = []
-    for i in filename:
-        for j in list_of_days:
-            pattern = re.compile(j)
-            list_of_days = pattern.search(i)
-            if list_of_days:
-                found_projects.append(i)
+# def get_valid_projects(list_of_days, filename):
+#     found_projects = []
+#     for i in filename:
+#         for j in list_of_days:
+#             pattern = re.compile(j)
+#             list_of_days = pattern.search(i)
+#             if list_of_days:
+#                 found_projects.append(i)
 
-    return found_projects
+    # return found_projects
 
-# found_projects = []
-# for i in args.f:
-#     for j in valid_dates:
-#         pattern = re.compile(j)
-#         valid_project = pattern.search(i)
-#         if valid_project:
-#             found_projects.append(i)
+found_projects = []
+for i in args.f:
+    for j in valid_dates:
+        pattern = re.compile(j)
+        valid_project = pattern.search(i)
+        if valid_project:
+            found_projects.append(i)
 
-found_projects = get_valid_projects(valid_dates, args.f)
+# found_projects = get_valid_projects(valid_dates, args.f)
 
 start_date = start_date.strftime("%Y-%m-%d")
 today = end_date.strftime("%Y-%m-%d")
